0e72ab80464e3a2435a50a3307f8a1940e87bcf3
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2023-02-05 01:47:11 +0600

    video thumbnail

--- components/Home/HowItWorksVideo.jsx
+++ components/Home/HowItWorksVideo.jsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
 import React, { useState } from 'react';
 import Container from '../common/Container';
 import HeadingText from '../UI/HeadingText';
 import SubHeadingText from '../UI/SubHeadingText';
+import Thumbnail from '../../assets/images/video-thumbnail.jpg';
 
 const HowItWorksVideo = () => {  
   return (
@@ -9,6 +11,7 @@ const HowItWorksVideo = () => {
       <div className='map-bg h-full pb-20'>
         <HeadingText text="How it works" />
         <SubHeadingText text="Here is a video how we operate our system" />
+        <Image src={Thumbnail} alt="image" />
 
         <div className='flex justify-center items-center mt-10'>
           <div className='flex max-w-[772px] h-[457px] border rounded-3xl overflow-hidden'>
