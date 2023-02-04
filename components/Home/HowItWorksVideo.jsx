ccf391df294ae2cd7eece0a94c33eba038316903
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2023-02-05 01:40:21 +0600

    video section added

---
+++ components/Home/HowItWorksVideo.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Container from '../common/Container';
+import HeadingText from '../UI/HeadingText';
+import SubHeadingText from '../UI/SubHeadingText';
+
+const HowItWorksVideo = () => {  
+  return (
+    <Container>
+      <div className='map-bg h-full pb-20'>
+        <HeadingText text="How it works" />
+        <SubHeadingText text="Here is a video how we operate our system" />
+
+        <div className='flex justify-center items-center mt-10'>
+          <div className='flex max-w-[772px] h-[457px] border rounded-3xl overflow-hidden'>
+            <video id="my-video" className="" preload="auto" poster="https://gopayra.com/_next/static/media/card-thumbnail.d246906f.webp" loop controls>
+              <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4" type='video/mp4' />
+            </video>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HowItWorksVideo;
--- components/common/Layout.jsx
+++ components/common/Layout.jsx
@@ -1,11 +1,17 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+import { CloudLightning } from 'react-feather';
 import Footer from './Footer';
-import Navbar from './Navbar2.0';
+import Navbar from './Navbar';
+import Navbar2 from './Navbar2.0';
 
 const Layout = ({ children }) => {
+  const route = useRouter();
+  
+  console.log(route.pathname)
   return (
     <div className='content'>
-      <Navbar />
+      {route.pathname === "/terms-and-conditions" || "/privacy-policy" ? <Navbar /> : <Navbar2 /> }
       {children}
       <Footer />
     </div>
