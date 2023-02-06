b38447190d4a2e376d1f85c46d479c78aede490c
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2023-02-06 23:41:50 +0600

    spacing the sections properly

--- components/Home/WhyChooseOurApps.jsx
+++ components/Home/WhyChooseOurApps.jsx
@@ -47,28 +47,30 @@ const WhyChooseOurApps = () => {
     points: "Historical data"
   }];
   return (
-    <Container>
-      <HeadingText text="Why choose our apps" />
-      <div className='flex justify-center flex-wrap gap-[10px] mt-12' data-aos="zoom-in">
+    <section className='mt-24'>
+      <Container>
+        <HeadingText text="Why choose our apps" />
+        <div className='flex justify-center flex-wrap gap-[10px] mt-12' data-aos="zoom-in">
 
-        {highlights.map((highlight, index) => {
-          return <div className='rounded-2xl border border-liteBorder pt-4 w-[219px] h-[120px] text-center mt-3 hover:border-gopayra hover:scale-105 transition-all cursor-pointer' key={index}>
-            <div className='bg-gopayra w-[32px] h-[32px] mx-auto rounded-full flex justify-center items-center'>
-              <Image src={highlight.icon} alt="" />
+          {highlights.map((highlight, index) => {
+            return <div className='rounded-2xl border border-liteBorder pt-4 w-[219px] h-[120px] text-center mt-3 hover:border-gopayra hover:scale-105 transition-all cursor-pointer' key={index}>
+              <div className='bg-gopayra w-[32px] h-[32px] mx-auto rounded-full flex justify-center items-center'>
+                <Image src={highlight.icon} alt="" />
+              </div>
+              <p className='font-medium mt-2'>{highlight.points}</p>
             </div>
-            <p className='font-medium mt-2'>{highlight.points}</p>
-          </div>
-        })}
+          })}
 
-      </div>
+        </div>
 
-      {/* see more button */}
-      <div className='text-center mt-12'>
-        <button className='py-4 px-7 bg-black text-white rounded-lg text-lg font-medium hover:bg-white border hover:border-black hover:text-black active:bg-gopayra active:text-white transition-all'>
-          See more
-        </button>
-      </div>
-    </Container>
+        {/* see more button */}
+        <div className='text-center mt-12'>
+          <button className='py-4 px-7 bg-black text-white rounded-lg text-lg font-medium hover:bg-white border hover:border-black hover:text-black active:bg-gopayra active:text-white transition-all'>
+            See more
+          </button>
+        </div>
+      </Container>
+    </section>
   );
 };
 
