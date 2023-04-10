6bed33bbcc29301ed249ec53f61910b3b0eab972
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2023-04-10 16:43:18 +0600

    added the video on the page

--- components/common/HowItWorksVideo.jsx
+++ components/common/HowItWorksVideo.jsx
@@ -17,7 +17,7 @@ const HowItWorksVideo = () => {
           <div className='flex justify-center items-center mt-10'>
             <div className='flex max-w-[772px] h-[457px] overflow-hidden'>
               <video id="my-video" className="rounded-3xl" preload="auto" poster="https://gopayra.com/_next/static/media/video-thumbnail.da698279.jpg" loop controls>
-                <source src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4" type='video/mp4' />
+                <source src="../../assets/images/how-it-works-video.mp4" type='video/mp4' />
               </video>
             </div>
           </div>
